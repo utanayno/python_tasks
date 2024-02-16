822a73e4862572b1d0be94f268cc856dc6a48e96
Юлия Танайно <utanayno@gmail.com>
2024-02-16 09:32:07 +0100

    Add files via upload

---
+++ lesson 8/EmployeeApi.py
@@ -0,0 +1,62 @@
+import requests
+class EmployeeApi:
+
+    def __init__(self, url):
+        self.url = url
+
+    #метод на сохранение пользовательского токена
+    def get_token(self, user = 'bloom', password = 'fire-fairy'):  # значения по умолчанию. Если ничего не указывать, то подставятся они. Если указать свои, то подставятся свои.
+        creds = {
+            'username' : user,
+            'password' : password
+        }
+    #авторизация
+        resp = requests.post(self.url+'/auth/login', json=creds)
+        return resp.json()["userToken"]
+
+    #метод на получение списка сотрудников для компании
+    def get_employee_list(self, company):
+        resp = requests.get(self.url+'/employee?company='+ str(company))
+        return resp.json()
+    
+    #метод на добавление сотрудника (все поля заполнены)
+    def create_employee(self, id, firstName, lastName, middleName, companyId, email, url, phone, birthdate, isActive):
+        employee = {
+            "id": id,
+            "firstName": firstName,
+            "lastName": lastName,
+            "middleName": middleName,
+            "companyId": companyId,
+            "email": email,
+            "url": url,
+            "phone": phone,
+            "birthdate": birthdate,
+            "isActive": isActive
+        }
+
+        my_headers = {}
+        my_headers["x-client-token"] = self.get_token()
+        resp = requests.post(self.url+'/employee', json=employee, headers=my_headers)
+        return resp.json()
+    
+     #метод на получение конкретного сотрудника
+    def get_employee(self, id):
+        resp = requests.get(self.url+'/employee/'+ str(id))
+        return resp.json()
+    
+    #метод на редактирование сотрудника
+    def edit_employee(self, new_id, new_lastname, new_email, new_url, new_phone, isActive):
+        my_headers = {}
+        my_headers["x-client-token"] = self.get_token()
+
+        employee = {
+                "lastName": new_lastname,
+                "email": new_email,
+                "url": new_url,
+                "phone": new_phone,
+                "isActive": isActive
+                }
+    
+
+        resp = requests.patch(self.url+'/employee/'+ str(new_id), headers=my_headers, json=employee)
+        return resp.json()
---
+++ lesson 8/test_x_clients_empl.py
@@ -0,0 +1,268 @@
+import requests 
+from CompanyApi import CompanyApi
+from EmployeeApi import EmployeeApi
+from faker import Faker
+fake = Faker()
+
+url = "https://x-clients-be.onrender.com"
+
+api_company = CompanyApi(url)
+api_employee = EmployeeApi(url)
+
+#1.получение списка сотрудников созданной компании
+def test_get_employees_empty_company():
+    #создаем компанию
+    name = "Skyeng_company"
+    descr = ''
+    result = api_company.create_company(name, descr)
+    company_id = result["id"]
+
+    # получить количество сотрудников
+    body = api_employee.get_employee_list(company_id)
+    #проверяем, что сотрудников нет
+    len_before = len(body)
+    assert len_before == 0
+
+#2.1. добавление сотрудника (все поля заполнены)
+def test_add_employee_valid():
+    #создаем компанию
+    name = "Skyeng_company"
+    descr = ''
+    result = api_company.create_company(name, descr)
+    company_id = result["id"]
+
+    # получить количество сотрудников
+    body = api_employee.get_employee_list(company_id)
+    #проверяем, что сотрудников нет
+    len_before = len(body)
+
+    # добавить нового сотрудника
+    id = 5
+    firstName = "Иван"
+    lastName = "Иванов"
+    middleName = "Иванович"
+    companyId = company_id
+    email = "test@mail.com"
+    url = "www.ivanov.com"
+    phone = "79232323"
+    birthdate = "2024-02-14T17:24:38.039Z"
+    isActive = True
+    result = api_employee.create_employee(id, firstName, lastName, middleName, companyId, email, url, phone, birthdate, isActive)
+    new_id = result["id"]
+
+    # получить количество сотрудников
+    body = api_employee.get_employee_list(company_id)
+    len_after = len(body)
+    
+    # проверить, что их +1
+    assert len_after - len_before == 1
+
+    # проверить, что id последнего сотрудника в списке равен ответу из шага 2
+    assert body[-1]["id"] == new_id
+
+#2.2. добавление сотрудника (обязательные поля не заполнены)
+def test_add_employee_no_valid():
+    #создаем компанию
+    name = "Skyeng_company"
+    descr = ''
+    result = api_company.create_company(name, descr)
+    company_id = result["id"]
+
+    # добавить нового сотрудника
+    id = 5
+    firstName = ""
+    lastName = ""
+    middleName = "Иванович"
+    companyId = company_id
+    email = "test@mail.com"
+    url = "www.ivanov.com"
+    phone = "79232323"
+    birthdate = "2024-02-14T17:24:38.039Z"
+    isActive = ""
+    result = api_employee.create_employee(id, firstName, lastName, middleName, companyId, email, url, phone, birthdate, isActive)
+    assert result["statusCode"] == 400
+    assert result["message"] == [
+        "firstName should not be empty",
+        "lastName should not be empty"
+    ]
+
+#2.3. добавление сотрудника (не указан ID компании)
+def test_add_employee_no_id_comp():
+    #создаем компанию
+    name = "Skyeng_company"
+    descr = ''
+    result = api_company.create_company(name, descr)
+    company_id = result["id"]
+
+    # добавить нового сотрудника
+    id = 6
+    firstName = "Иван"
+    lastName = "Иванов"
+    middleName = "Иванович"
+    companyId = ""
+    email = "test@mail.com"
+    url = "www.ivanov.com"
+    phone = "79232323"
+    birthdate = "2024-02-14T17:24:38.039Z"
+    isActive = True
+    result = api_employee.create_employee(id, firstName, lastName, middleName, companyId, email, url, phone, birthdate, isActive)
+    assert result["statusCode"] == 400
+    
+
+#3.получить сотрудника по ID
+def test_get_employee():
+    #создаем компанию
+    name = "Skyeng_company"
+    descr = ''
+    result = api_company.create_company(name, descr)
+    company_id = result["id"]
+
+    # добавить нового сотрудника
+    id = 6
+    firstName = "Иван"
+    lastName = "Иванов"
+    middleName = "Иванович"
+    companyId = company_id
+    email = "test@mail.com"
+    url = "www.ivanov.com"
+    phone = "79232323"
+    birthdate = "2024-02-14T17:24:38.039Z"
+    isActive = True
+    result = api_employee.create_employee(id, firstName, lastName, middleName, companyId, email, url, phone, birthdate, isActive)
+    new_id = result["id"]
+    #получить сотрудника по ID
+    new_employee = api_employee.get_employee(new_id)
+
+    assert new_employee["id"] == new_id
+    assert new_employee["firstName"] == firstName
+    assert new_employee["lastName"] == lastName
+    assert new_employee["companyId"] == companyId
+    assert new_employee["isActive"] == isActive
+
+#4.редактирование сотрудника
+def test_edit_employee():
+    #создаем компанию
+    name = "Skyeng_company"
+    descr = ''
+    result = api_company.create_company(name, descr)
+    company_id = result["id"]
+
+    # добавить нового сотрудника
+    id = 6
+    firstName = "Иван"
+    lastName = "Иванов"
+    middleName = "Иванович"
+    companyId = company_id
+    email = "test@mail.com"
+    url = "www.ivanov.com"
+    phone = "79232323"
+    birthdate = "2024-02-14T17:24:38.039Z"
+    isActive = True
+    result = api_employee.create_employee(id, firstName, lastName, middleName, companyId, email, url, phone, birthdate, isActive)
+    new_id = result["id"]
+
+    #редактируем сотрудника
+    new_lastname = "Петров"
+    new_email = "test@mail.com"
+    new_url = "www.petrov.com"
+    new_phone = "79232323"
+    new_isActive = True
+    edited = api_employee.edit_employee(new_id, new_lastname, new_email, new_url, new_phone, isActive)
+    assert edited["id"] == new_id
+    assert edited["email"] == new_email
+    assert edited["url"] == new_url
+    assert edited["isActive"] == new_isActive
+
+
+
+#Если нам нужно поработать с компанией по ID (не создавая каждый раз новую)
+
+# указываем ID компании
+company_id = 431
+
+def test_get_employees():
+    # получить количество сотрудников
+    body = api_employee.get_employee_list(company_id)
+    assert len(body) > 0
+
+def test_add_employee_2():
+    # получить количество сотрудников
+    body = api_employee.get_employee_list(company_id)
+    len_before = len(body)
+
+    # добавить нового сотрудника
+    id = 5
+    firstName = "Иван"
+    lastName = "Иванов"
+    middleName = "Иванович"
+    companyId = company_id
+    email = "test@mail.com"
+    url = "www.ivanov.com"
+    phone = "79232323"
+    birthdate = "2024-02-14T17:24:38.039Z"
+    isActive = True
+    result = api_employee.create_employee(id, firstName, lastName, middleName, companyId, email, url, phone, birthdate, isActive)
+    new_id = result["id"]
+
+    # получить количество сотрудников
+    body = api_employee.get_employee_list(company_id)
+    len_after = len(body)
+    
+    # проверить, что их +1
+    assert len_after - len_before == 1
+
+    # проверить, что id последнего сотрудника в списке равен ответу из шага 2
+    assert body[-1]["id"] == new_id
+
+
+def test_get_employee_2():
+    # добавить нового сотрудника
+    id = 6
+    firstName = "Иван"
+    lastName = "Иванов"
+    middleName = "Иванович"
+    companyId = company_id
+    email = "test@mail.com"
+    url = "www.ivanov.com"
+    phone = "79232323"
+    birthdate = "2024-02-14T17:24:38.039Z"
+    isActive = True
+    result = api_employee.create_employee(id, firstName, lastName, middleName, companyId, email, url, phone, birthdate, isActive)
+    new_id = result["id"]
+    #получить сотрудника по ID
+    new_employee = api_employee.get_employee(new_id)
+
+    assert new_employee["id"] == new_id
+    assert new_employee["firstName"] == firstName
+    assert new_employee["lastName"] == lastName
+    assert new_employee["companyId"] == companyId
+    assert new_employee["isActive"] == isActive
+
+#4.редактирование сотрудника
+def test_edit_employee_2():
+    # добавить нового сотрудника
+    id = 6
+    firstName = fake.name()
+    lastName = "Иванов"
+    middleName = "Иванович"
+    companyId = company_id
+    email = "test@mail.com"
+    url = "www.ivanov.com"
+    phone = "79232323"
+    birthdate = "2024-02-14T17:24:38.039Z"
+    isActive = True
+    result = api_employee.create_employee(id, firstName, lastName, middleName, companyId, email, url, phone, birthdate, isActive)
+    new_id = result["id"]
+
+    #редактируем сотрудника
+    new_lastname = "Петров"
+    new_email = "test@mail.com"
+    new_url = "www.petrov.com"
+    new_phone = "79232323"
+    new_isActive = True
+    edited = api_employee.edit_employee(new_id, new_lastname, new_email, new_url, new_phone, isActive)
+    assert edited["id"] == new_id
+    assert edited["email"] == new_email
+    assert edited["url"] == new_url
+    assert edited["isActive"] == new_isActive
+
